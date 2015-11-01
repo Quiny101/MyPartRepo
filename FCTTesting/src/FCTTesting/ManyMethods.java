db1355cccad2d6119f5ab82ce9f47a6d3628d6df
Rudolf Stander <rudolfstan@gmail.com>
2015-11-01 21:17:31 +0200

    Added Function Call Tracker Testing Repo

---
+++ FCTTesting/src/FCTTesting/ManyMethods.java
@@ -0,0 +1,728 @@
+package FCTTesting;
+
+public class ManyMethods {
+	public static void method0() {
+		return;
+	}
+	public static void method1() {
+		return;
+	}
+	public static void method2() {
+		return;
+	}
+	public static void method3() {
+		return;
+	}
+	public static void method4() {
+		return;
+	}
+	public static void method5() {
+		return;
+	}
+	public static void method6() {
+		return;
+	}
+	public static void method7() {
+		return;
+	}
+	public static void method8() {
+		return;
+	}
+	public static void method9() {
+		return;
+	}
+	public static void method10() {
+		return;
+	}
+	public static void method11() {
+		return;
+	}
+	public static void method12() {
+		return;
+	}
+	public static void method13() {
+		return;
+	}
+	public static void method14() {
+		return;
+	}
+	public static void method15() {
+		return;
+	}
+	public static void method16() {
+		return;
+	}
+	public static void method17() {
+		return;
+	}
+	public static void method18() {
+		return;
+	}
+	public static void method19() {
+		return;
+	}
+	public static void method20() {
+		return;
+	}
+	public static void method21() {
+		return;
+	}
+	public static void method22() {
+		return;
+	}
+	public static void method23() {
+		return;
+	}
+	public static void method24() {
+		return;
+	}
+	public static void method25() {
+		return;
+	}
+	public static void method26() {
+		return;
+	}
+	public static void method27() {
+		return;
+	}
+	public static void method28() {
+		return;
+	}
+	public static void method29() {
+		return;
+	}
+	public static void method30() {
+		return;
+	}
+	public static void method31() {
+		return;
+	}
+	public static void method32() {
+		return;
+	}
+	public static void method33() {
+		return;
+	}
+	public static void method34() {
+		return;
+	}
+	public static void method35() {
+		return;
+	}
+	public static void method36() {
+		return;
+	}
+	public static void method37() {
+		return;
+	}
+	public static void method38() {
+		return;
+	}
+	public static void method39() {
+		return;
+	}
+	public static void method40() {
+		return;
+	}
+	public static void method41() {
+		return;
+	}
+	public static void method42() {
+		return;
+	}
+	public static void method43() {
+		return;
+	}
+	public static void method44() {
+		return;
+	}
+	public static void method45() {
+		return;
+	}
+	public static void method46() {
+		return;
+	}
+	public static void method47() {
+		return;
+	}
+	public static void method48() {
+		return;
+	}
+	public static void method49() {
+		return;
+	}
+	public static void method50() {
+		return;
+	}
+	public static void method51() {
+		return;
+	}
+	public static void method52() {
+		return;
+	}
+	public static void method53() {
+		return;
+	}
+	public static void method54() {
+		return;
+	}
+	public static void method55() {
+		return;
+	}
+	public static void method56() {
+		return;
+	}
+	public static void method57() {
+		return;
+	}
+	public static void method58() {
+		return;
+	}
+	public static void method59() {
+		return;
+	}
+	public static void method60() {
+		return;
+	}
+	public static void method61() {
+		return;
+	}
+	public static void method62() {
+		return;
+	}
+	public static void method63() {
+		return;
+	}
+	public static void method64() {
+		return;
+	}
+	public static void method65() {
+		return;
+	}
+	public static void method66() {
+		return;
+	}
+	public static void method67() {
+		return;
+	}
+	public static void method68() {
+		return;
+	}
+	public static void method69() {
+		return;
+	}
+	public static void method70() {
+		return;
+	}
+	public static void method71() {
+		return;
+	}
+	public static void method72() {
+		return;
+	}
+	public static void method73() {
+		return;
+	}
+	public static void method74() {
+		return;
+	}
+	public static void method75() {
+		return;
+	}
+	public static void method76() {
+		return;
+	}
+	public static void method77() {
+		return;
+	}
+	public static void method78() {
+		return;
+	}
+	public static void method79() {
+		return;
+	}
+	public static void method80() {
+		return;
+	}
+	public static void method81() {
+		return;
+	}
+	public static void method82() {
+		return;
+	}
+	public static void method83() {
+		return;
+	}
+	public static void method84() {
+		return;
+	}
+	public static void method85() {
+		return;
+	}
+	public static void method86() {
+		return;
+	}
+	public static void method87() {
+		return;
+	}
+	public static void method88() {
+		return;
+	}
+	public static void method89() {
+		return;
+	}
+	public static void method90() {
+		return;
+	}
+	public static void method91() {
+		return;
+	}
+	public static void method92() {
+		return;
+	}
+	public static void method93() {
+		return;
+	}
+	public static void method94() {
+		return;
+	}
+	public static void method95() {
+		return;
+	}
+	public static void method96() {
+		return;
+	}
+	public static void method97() {
+		return;
+	}
+	public static void method98() {
+		return;
+	}
+	public static void method99() {
+		return;
+	}
+	public static void method100() {
+		return;
+	}
+	public static void method101() {
+		return;
+	}
+	public static void method102() {
+		return;
+	}
+	public static void main(String args[])
+	{
+		for (int i = 0; i < 1*1000; i++) {
+			method0();
+		}
+
+		for (int i = 0; i < 2*1000; i++) {
+			method1();
+		}
+
+		for (int i = 0; i < 3*1000; i++) {
+			method2();
+		}
+
+		for (int i = 0; i < 4*1000; i++) {
+			method3();
+		}
+
+		for (int i = 0; i < 5*1000; i++) {
+			method4();
+		}
+
+		for (int i = 0; i < 6*1000; i++) {
+			method5();
+		}
+
+		for (int i = 0; i < 7*1000; i++) {
+			method6();
+		}
+
+		for (int i = 0; i < 8*1000; i++) {
+			method7();
+		}
+
+		for (int i = 0; i < 9*1000; i++) {
+			method8();
+		}
+
+		for (int i = 0; i < 10*1000; i++) {
+			method9();
+		}
+
+		for (int i = 0; i < 11*1000; i++) {
+			method10();
+		}
+
+		for (int i = 0; i < 12*1000; i++) {
+			method11();
+		}
+
+		for (int i = 0; i < 13*1000; i++) {
+			method12();
+		}
+
+		for (int i = 0; i < 14*1000; i++) {
+			method13();
+		}
+
+		for (int i = 0; i < 15*1000; i++) {
+			method14();
+		}
+
+		for (int i = 0; i < 16*1000; i++) {
+			method15();
+		}
+
+		for (int i = 0; i < 17*1000; i++) {
+			method16();
+		}
+
+		for (int i = 0; i < 18*1000; i++) {
+			method17();
+		}
+
+		for (int i = 0; i < 19*1000; i++) {
+			method18();
+		}
+
+		for (int i = 0; i < 20*1000; i++) {
+			method19();
+		}
+
+		for (int i = 0; i < 21*1000; i++) {
+			method20();
+		}
+
+		for (int i = 0; i < 22*1000; i++) {
+			method21();
+		}
+
+		for (int i = 0; i < 23*1000; i++) {
+			method22();
+		}
+
+		for (int i = 0; i < 24*1000; i++) {
+			method23();
+		}
+
+		for (int i = 0; i < 25*1000; i++) {
+			method24();
+		}
+
+		for (int i = 0; i < 26*1000; i++) {
+			method25();
+		}
+
+		for (int i = 0; i < 27*1000; i++) {
+			method26();
+		}
+
+		for (int i = 0; i < 28*1000; i++) {
+			method27();
+		}
+
+		for (int i = 0; i < 29*1000; i++) {
+			method28();
+		}
+
+		for (int i = 0; i < 30*1000; i++) {
+			method29();
+		}
+
+		for (int i = 0; i < 31*1000; i++) {
+			method30();
+		}
+
+		for (int i = 0; i < 32*1000; i++) {
+			method31();
+		}
+
+		for (int i = 0; i < 33*1000; i++) {
+			method32();
+		}
+
+		for (int i = 0; i < 34*1000; i++) {
+			method33();
+		}
+
+		for (int i = 0; i < 35*1000; i++) {
+			method34();
+		}
+
+		for (int i = 0; i < 36*1000; i++) {
+			method35();
+		}
+
+		for (int i = 0; i < 37*1000; i++) {
+			method36();
+		}
+
+		for (int i = 0; i < 38*1000; i++) {
+			method37();
+		}
+
+		for (int i = 0; i < 39*1000; i++) {
+			method38();
+		}
+
+		for (int i = 0; i < 40*1000; i++) {
+			method39();
+		}
+
+		for (int i = 0; i < 41*1000; i++) {
+			method40();
+		}
+
+		for (int i = 0; i < 42*1000; i++) {
+			method41();
+		}
+
+		for (int i = 0; i < 43*1000; i++) {
+			method42();
+		}
+
+		for (int i = 0; i < 44*1000; i++) {
+			method43();
+		}
+
+		for (int i = 0; i < 45*1000; i++) {
+			method44();
+		}
+
+		for (int i = 0; i < 46*1000; i++) {
+			method45();
+		}
+
+		for (int i = 0; i < 47*1000; i++) {
+			method46();
+		}
+
+		for (int i = 0; i < 48*1000; i++) {
+			method47();
+		}
+
+		for (int i = 0; i < 49*1000; i++) {
+			method48();
+		}
+
+		for (int i = 0; i < 50*1000; i++) {
+			method49();
+		}
+
+		for (int i = 0; i < 51*1000; i++) {
+			method50();
+		}
+
+		for (int i = 0; i < 52*1000; i++) {
+			method51();
+		}
+
+		for (int i = 0; i < 53*1000; i++) {
+			method52();
+		}
+
+		for (int i = 0; i < 54*1000; i++) {
+			method53();
+		}
+
+		for (int i = 0; i < 55*1000; i++) {
+			method54();
+		}
+
+		for (int i = 0; i < 56*1000; i++) {
+			method55();
+		}
+
+		for (int i = 0; i < 57*1000; i++) {
+			method56();
+		}
+
+		for (int i = 0; i < 58*1000; i++) {
+			method57();
+		}
+
+		for (int i = 0; i < 59*1000; i++) {
+			method58();
+		}
+
+		for (int i = 0; i < 60*1000; i++) {
+			method59();
+		}
+
+		for (int i = 0; i < 61*1000; i++) {
+			method60();
+		}
+
+		for (int i = 0; i < 62*1000; i++) {
+			method61();
+		}
+
+		for (int i = 0; i < 63*1000; i++) {
+			method62();
+		}
+
+		for (int i = 0; i < 64*1000; i++) {
+			method63();
+		}
+
+		for (int i = 0; i < 65*1000; i++) {
+			method64();
+		}
+
+		for (int i = 0; i < 66*1000; i++) {
+			method65();
+		}
+
+		for (int i = 0; i < 67*1000; i++) {
+			method66();
+		}
+
+		for (int i = 0; i < 68*1000; i++) {
+			method67();
+		}
+
+		for (int i = 0; i < 69*1000; i++) {
+			method68();
+		}
+
+		for (int i = 0; i < 70*1000; i++) {
+			method69();
+		}
+
+		for (int i = 0; i < 71*1000; i++) {
+			method70();
+		}
+
+		for (int i = 0; i < 72*1000; i++) {
+			method71();
+		}
+
+		for (int i = 0; i < 73*1000; i++) {
+			method72();
+		}
+
+		for (int i = 0; i < 74*1000; i++) {
+			method73();
+		}
+
+		for (int i = 0; i < 75*1000; i++) {
+			method74();
+		}
+
+		for (int i = 0; i < 76*1000; i++) {
+			method75();
+		}
+
+		for (int i = 0; i < 77*1000; i++) {
+			method76();
+		}
+
+		for (int i = 0; i < 78*1000; i++) {
+			method77();
+		}
+
+		for (int i = 0; i < 79*1000; i++) {
+			method78();
+		}
+
+		for (int i = 0; i < 80*1000; i++) {
+			method79();
+		}
+
+		for (int i = 0; i < 81*1000; i++) {
+			method80();
+		}
+
+		for (int i = 0; i < 82*1000; i++) {
+			method81();
+		}
+
+		for (int i = 0; i < 83*1000; i++) {
+			method82();
+		}
+
+		for (int i = 0; i < 84*1000; i++) {
+			method83();
+		}
+
+		for (int i = 0; i < 85*1000; i++) {
+			method84();
+		}
+
+		for (int i = 0; i < 86*1000; i++) {
+			method85();
+		}
+
+		for (int i = 0; i < 87*1000; i++) {
+			method86();
+		}
+
+		for (int i = 0; i < 88*1000; i++) {
+			method87();
+		}
+
+		for (int i = 0; i < 89*1000; i++) {
+			method88();
+		}
+
+		for (int i = 0; i < 90*1000; i++) {
+			method89();
+		}
+
+		for (int i = 0; i < 91*1000; i++) {
+			method90();
+		}
+
+		for (int i = 0; i < 92*1000; i++) {
+			method91();
+		}
+
+		for (int i = 0; i < 93*1000; i++) {
+			method92();
+		}
+
+		for (int i = 0; i < 94*1000; i++) {
+			method93();
+		}
+
+		for (int i = 0; i < 95*1000; i++) {
+			method94();
+		}
+
+		for (int i = 0; i < 96*1000; i++) {
+			method95();
+		}
+
+		for (int i = 0; i < 97*1000; i++) {
+			method96();
+		}
+
+		for (int i = 0; i < 98*1000; i++) {
+			method97();
+		}
+
+		for (int i = 0; i < 99*1000; i++) {
+			method98();
+		}
+
+		for (int i = 0; i < 100*1000; i++) {
+			method99();
+		}
+
+		for (int i = 0; i < 101*1000; i++) {
+			method100();
+		}
+
+		for (int i = 0; i < 102*1000; i++) {
+			method101();
+		}
+
+		for (int i = 0; i < 103*1000; i++) {
+			method102();
+		}
+
+	}
+}
