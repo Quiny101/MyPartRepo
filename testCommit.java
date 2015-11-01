96e4ee367cabb1a0327f16e427ff1558ae4afbb2
jlwesthuizen <jl.westhuizen@gmail.com>
2015-11-01 22:52:04 +0200

    Last test before sleep

--- testCommit.java
+++ testCommit.java
@@ -1,5 +1,6 @@
 public class testCommit {
 	public static void main(String [] args){
-		System.out.println();
+		System.out.println("Added to line 3");
+		System.out.println("Added to line 4");
 	}
 }
